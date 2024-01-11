2b1481698388b3bffb5a655cbb1a4f5d11432a18
ZoreAditya <113139336+ZoreAditya@users.noreply.github.com>
2024-01-11 13:43:10 +0530

    Update NumberGame_Task1.java

--- NumberGuessingGame/src/NumberGame_Task1.java
+++ NumberGuessingGame/src/NumberGame_Task1.java
@@ -15,21 +15,16 @@ public class NumberGame_Task1 {
 
 
         while (true) {
-                    // Step 1: Generate a random number within the specified range
                     int targetNumber = random.nextInt(100) + 1;
 
-                    // Step 5: Set the maximum number of attempts
                     int maxAttempts = 10;
                     int attempts = 0;
 
-                    // Step 6: Allow the user to play again
                     while (attempts < maxAttempts) {
-                        // Step 2: Prompt the user to enter their guess
 
                         System.out.print("Guess the number between 1 and 100: ");
                         int userGuess = scanner.nextInt();
 
-                        // Step 3: Compare the user's guess with the generated number
                         if (userGuess == targetNumber) {
                             score=score+10;
                             System.out.println("Congratulations! You guessed the correct number " + targetNumber + " in " + (attempts + 1) + " attempts."
@@ -59,7 +54,6 @@ public class NumberGame_Task1 {
                         System.out.println("Your score:"+score);
                     }
 
-                    // Step 7: Display the user's score
                     System.out.print("Do you want to play again? (yes/no): ");
                     String playAgain = scanner.next().toLowerCase();
                     if (!playAgain.equals("yes")) {
